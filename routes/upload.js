21c3770ccee86fb40a8f609852ca13b8086a7e3d
David Lay <davidlaym@gmail.com>
2018-03-18 18:26:40 -0300

    upload post action

--- routes/upload.js
+++ routes/upload.js
@@ -1,9 +1,29 @@
 var express = require('express')
+var fs = require('fs')
+var path = require('path')
+
+var multer = require('multer')({
+  dest: 'public/uploads'
+})
+
 var router = express.Router()
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
   res.render('uploadForm.ejs')
 })
+
+router.post('/', [multer.single('attachment')], function (req, res, next) {
+  var {fileName} = storeWithOriginalName(req.file)
+  res.render('uploadOK.ejs', {fileName})
+})
+
+function storeWithOriginalName (file) {
+  var fullNewPath = path.join(file.destination, file.originalname)
+  fs.renameSync(file.path, fullNewPath)
+
+  return {
+    fileName: file.originalname
+  }
+}
 
 module.exports = router
