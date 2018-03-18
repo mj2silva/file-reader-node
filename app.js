a9c573b8ed73033b3a64b6d208c55a531a7469a6
David Lay <davidlaym@gmail.com>
2018-03-18 17:49:09 -0300

    upload form and route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('morgan')
 var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
-var users = require('./routes/users')
+var upload = require('./routes/upload')
 
 var app = express()
 
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
-app.use('/users', users)
+app.use('/upload', upload)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+  res.render('uploadForm.ejs')
 })
 
 module.exports = router
