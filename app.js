b40b1ed892f485877564c93cbbd0e8086f18d026
David Lay <davidlaym@gmail.com>
2018-03-18 17:39:51 -0300

    removed innecesary packages

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var path = require('path')
 var logger = require('morgan')
-var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
 var index = require('./routes/index')
@@ -16,7 +15,6 @@ app.set('view engine', 'ejs')
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
